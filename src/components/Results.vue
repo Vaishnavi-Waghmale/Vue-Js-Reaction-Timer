c948aa039205cbdeea79a1c8fd40b60bb4c88e74
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-10-22 22:35:38 +0530

    Result

---
+++ src/components/Results.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="result">
+        <p>Reaction Time - {{score}} ms</p>
+        <p class="rank">{{rank}}</p>
+    </div>
+
+
+</template>
+
+
+<script>
+
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Great Reflexes'
+        }
+        else if (this.score < 400) {
+            this.rank = 'Good job, quite fast'
+        }
+        else if (this.score < 1000) {
+            this.rank = 'Nicely done!'
+        }
+        else {
+            this.rank = 'Ohh You need to be attentive'
+        }
+    }
+}
+
+</script>
+
+
+<style>
+#result {
+    width: 400px;
+    border-radius: 20px;
+    background: rgb(46, 1, 8);
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+
+.rank {
+    font-size: 1.4em;
+    font-weight: bold;
+}
+</style>
